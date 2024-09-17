25770cd49e129d28ae4beedf507bbd6e658ce016
Resins <144451099+Resinss@users.noreply.github.com>
2024-09-17 14:53:44 +0200

    Add files via upload

---
+++ 17_09_2024/script.js
@@ -0,0 +1,18 @@
+let firstNumber;
+let secondNumber;
+let wynik;
+
+firstNumber = prompt("Podaj pierwsza liczbe");
+secondNumber = prompt("Podaj druga liczbe");
+wynik = firstNumber + secondNumber;
+document.write("Wynik dodawania wynosi:");
+document.write(wynik)
+wynik = firstNumber - secondNumber;
+document.write("Wynik odejmowania wynosi:");
+document.write(wynik)
+wynik = firstNumber * secondNumber;
+document.write("Wynik mnozenia wynosi:");
+document.write(wynik)
+wynik = firstNumber / secondNumber;
+document.write("Wynik dzielenia wynosi:");
+document.write(wynik)
